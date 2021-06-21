9be6ae4a2f96890b09d9d278da2f22408c24dab4
GoldenPavilion <rcorystine@gmail.com>
2021-06-21 13:56:17 -0400

    set up strong params in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,10 @@ class UsersController < ApplicationController
 
     def show
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :author_name, :email, :password, :password_confirmation)
+    end
 end
