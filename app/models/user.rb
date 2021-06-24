d1a3cf9075ac6538506ae0f655a3c7141475b989
GoldenPavilion <rcorystine@gmail.com>
2021-06-24 18:37:26 -0400

    added author_name and email to validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
+    validates :author_name, presence: true
+    validates :email, presence: true, uniqueness: true
 
     has_secure_password
 
