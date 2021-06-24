f8371d89dd21c3e7c91c27770c9b14ee156b080c
GoldenPavilion <rcorystine@gmail.com>
2021-06-24 18:32:10 -0400

    added username validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+
     has_secure_password
 
     has_many :stories
     has_many :worlds, through: :stories
+
 end
