0e8b6fe5ccf0d5c6187f28d3b5bb78bd7cbd5ef6
GoldenPavilion <rcorystine@gmail.com>
2021-06-25 17:27:50 -0400

    added validations to character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,8 @@
 class Character < ApplicationRecord
+    validates :name, presence: true
+    validates :world_id, presence: true
+    validates :story_id, presence: true
+    
     belongs_to :world
     belongs_to :story
 end
