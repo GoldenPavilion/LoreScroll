87c2b3f7e9c379457b8bbd1a6d10b47a8ec92ede
GoldenPavilion <rcorystine@gmail.com>
2021-07-14 13:38:48 -0400

    added storieshelper module with field_completed? method

---
+++ app/helpers/stories_helper.rb
@@ -0,0 +1,11 @@
+module StoriesHelper
+
+    def field_completed?(object, attribute, condition)
+        if object.send(attribute).send(condition)
+            content_tag :p, "Incomplete"
+        else
+            content_tag :p, object.send(attribute)
+        end
+    end
+
+end
